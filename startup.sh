4ae0fed5c28ce934b101e072003ff1452d9f9a7d
rgwch <weirich@elexis.ch>
2015-07-27 13:47:02 +0200

    start

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 cp webelexis/webelexis-server/target/webelexis-server-${VERSION}.jar .
-java -jar webelexis-server-${VERSION}.jar cfglocal.json >webelexis.log
+java -jar webelexis-server-${VERSION}.jar cfglocal.json >>webelexos.log 2>&1
